f5305520ec9651684317716852b8e203aabb5b83
Jimmy <jimmy.hsueh@parivedasolutions.com>
2021-02-01 13:32:58 -0800

    adding dependencies and unit tests (@jhsueh)

--- src/main/java/pariveda/osdu/OsduBaseClient.java
+++ src/main/java/pariveda/osdu/OsduBaseClient.java
@@ -1,5 +1,7 @@
 package pariveda.osdu;
 
+import java.net.URL;
+
 import okhttp3.*;
 
 public class OsduBaseClient {
@@ -9,16 +11,36 @@ public class OsduBaseClient {
 	OkHttpClient client;
 	MediaType mediaType;
 	
-	OsduBaseClient(){
-		System.out.println("url, dataparition, contenttype are required");
+	public OsduBaseClient(String url, String dataPartition, String contentType){
+		boolean isValidUrl = validURL(url);
+		boolean isValidContentType = validContentType(contentType);
+		if (isValidUrl && isValidContentType) {
+			this.url = url;
+			this.dataPartition = dataPartition;
+			this.contentType = contentType;		
+			this.client = new OkHttpClient().newBuilder().build();
+			this.mediaType = MediaType.parse(this.contentType);
+		} else {
+			String exceptionMessage = "";
+			if (!isValidUrl) exceptionMessage+= "Provided URL \"" + url + "\" is not valid\n";
+			if (!isValidContentType) exceptionMessage+= "Provided Content Type \"" + contentType + "\" is not valid\n";
+			throw new IllegalArgumentException(exceptionMessage);
+		}
 	}
-	
-	OsduBaseClient(String my_url, String my_datapartition, String my_contentType){
-		this.url = my_url;
-		this.dataPartition = my_datapartition;
-		this.contentType = my_contentType;		
-		this.client = new OkHttpClient().newBuilder().build();
-		this.mediaType = MediaType.parse(this.contentType);
+
+	private boolean validURL(String url) {
+		try {
+			new URL(url).toURI();
+			return true;
+		}
+		catch (Exception e) {
+			return false;
+		}
+	}
+
+	private boolean validContentType(String contentType) {
+		if (MediaType.parse(contentType)!=null) return true;
+		else return false;
 	}
 	
 }
--- src/main/java/pariveda/osdu/OsduSimpleClient.java
+++ src/main/java/pariveda/osdu/OsduSimpleClient.java
@@ -9,30 +9,44 @@ public class OsduSimpleClient extends OsduBaseClient{
 	//Bring your own token client, gets called in the app.java
 	String token;
 	
-	OsduSimpleClient(String my_url, String my_datapartition, String my_contentType, String my_token){
+	public OsduSimpleClient(String my_url, String my_datapartition, String my_contentType, String my_token){
 		super(my_url, my_datapartition, my_contentType);
 		this.token = my_token;
 	}
 
 	public JSONObject Search(String query) throws IOException{
 		System.out.println("OSDU Search.. Query: "+ query);
-		RequestBody body = RequestBody.create(this.mediaType, query);
-	    Request request = new Request.Builder()
-	      .url(this.url+"/api/search/v2/query")
-	      .method("POST", body)
-	      .addHeader("Content-Type", this.contentType)
-	      .addHeader("data-partition-id", this.dataPartition)
-	      .addHeader("Authorization", this.token)
-	      .build();
-	    
-	    Response response = this.client.newCall(request).execute();
-	    JSONObject resp = new JSONObject(response.body().string());
-	    //System.out.println("Resp in OSDU Simple Client: " +resp);
-	    return resp;
+		if(isValidSearchQuery(query)) {
+			RequestBody body = RequestBody.create(query, this.mediaType);
+			Request request = new Request.Builder()
+			.url(this.url+"/api/search/v2/query")
+			.method("POST", body)
+			.addHeader("Content-Type", this.contentType)
+			.addHeader("data-partition-id", this.dataPartition)
+			.addHeader("Authorization", this.token)
+			.build();
+			
+			Response response = this.client.newCall(request).execute();
+			JSONObject resp = new JSONObject(response.body().string());
+			//System.out.println("Resp in OSDU Simple Client: " +resp);
+			return resp;
+		}
+		else {
+			throw new IOException("Provided Search Query is invalid");
+		}
 	}
+
+	protected boolean isValidSearchQuery(String query) throws JSONException, IllegalArgumentException {
+		JSONObject queryJson = new JSONObject(query);
+		if( queryJson.getString("kind").matches("*:*:*:*") ) {
+			return true;
+		}
+		return false;
+	}
+
 	public JSONObject Delivery(String query) throws IOException{
 		System.out.println("OSDU Delivery.. Query: "+ query);
-		RequestBody body = RequestBody.create(this.mediaType, query);
+		RequestBody body = RequestBody.create(query, this.mediaType);
 	    Request request = new Request.Builder()
 	      .url(this.url+"/api/delivery/v2/GetFileSignedUrl")
 	      .method("POST", body)
